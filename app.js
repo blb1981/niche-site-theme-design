e1e96d15b636aefa4b87a1729ddce3fca23b29d9
Brandon Bland <brandonbland90@gmail.com>
2023-02-10 02:48:21 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// Reference for this setup: https://raddy.dev/blog/nodejs-express-layouts-and-partials/
+
+const path = require("path");
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
+const app = express();
+const port = 5000;
+
+// Static Files
+
+app.use(express.static("public"));
+
+// Template Engine Settings
+// Default layout is the frontend.ejs file
+// Other templates below will use different layouts, specified in the route
+
+app.use(expressLayouts);
+app.set("layout", "./layouts/frontend.ejs");
+app.set("view engine", "ejs");
+
+// Routes... Add future pages here
+// Frontend pages
+
+const details = {
+  siteName: 'Pilot Headquarters'
+}
+
+app.get("", (req, res) => {
+  res.render("index", { title: "Home Page", ...details });
+});
+
+// Server
+
+app.listen(port, () => {
+  console.log("Listening on http://localhost:5000");
+});
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Niche Site Theme Design
+
+A Wordpress theme designed to optimize display ads
+
+Inspired by [this theme](https://themeforest.net/item/admania-best-ad-optimized-wordpress-theme-for-adsense-affiliate-enthusiasts/18194026?irgwc=1&clickid=WUTXEV2GjxyNRFW0Vm26Pw3CUkA3KPXGPQJv2Q0&iradid=275988&irpid=102006&iradtype=ONLINE_TRACKING_LINK&irmptype=mediapartner&mp_value1=&utm_campaign=af_impact_radius_102006&utm_medium=affiliate&utm_source=impact_radius).
